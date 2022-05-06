07d4fb4b2b0e989fe2d0ff85aa305f79c1bc7ac4
S5233405 <101658438+S5233405@users.noreply.github.com>
2022-05-06 13:10:39 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  char n;
+    cin >> n;
+    if (number(n)==1 && n>=97 && n<=122){
+        cout << char (n-32);
+    } else if (number(n)==1 && n>=65 && n<=90 ){
+        cout << char (n+32);
+    } else {
+        cout <<"errore";
+    }
   return 0;
 }
