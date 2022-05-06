990697abd4b951875929612e1edb43d888f283ce
S5233405 <101658438+S5233405@users.noreply.github.com>
2022-05-06 13:02:42 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,7 +1,7 @@
 #include "lib.h"
 
 bool number (int x){
-    if (x>=65 && x<=90 | x>=97 && x<=122){
+    if ((x>=65 && x<=90) | (x>=97 && x<=122)){
         return true;
     } else {
         return false;
